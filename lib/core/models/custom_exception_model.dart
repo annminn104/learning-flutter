59e33cadec83146c5833e26f5e4fe0f641f7383a
annminn104 <annminn100@gmail.com>
2023-08-31 16:24:53 +0700

    feat: update resource

---
+++ lib/core/models/custom_exception_model.dart
@@ -0,0 +1,121 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import '../enums/exception_enum.dart';
+
+class CustomException implements Exception {
+  final String name, message;
+  final String? code;
+  final int? statusCode;
+  final ExceptionType exceptionType;
+
+  CustomException({
+    this.code,
+    int? statusCode,
+    required this.message,
+    this.exceptionType = ExceptionType.ApiException,
+  })  : statusCode = statusCode ?? 500,
+        name = exceptionType.name;
+
+  factory CustomException.fromDioException(Exception error) {
+    try {
+      if (error is DioError) {
+        switch (error.type) {
+          case DioErrorType.cancel:
+            return CustomException(
+              exceptionType: ExceptionType.CancelException,
+              statusCode: error.response?.statusCode,
+              message: 'Request cancelled prematurely',
+            );
+          case DioErrorType.connectionTimeout:
+            return CustomException(
+              exceptionType: ExceptionType.ConnectTimeoutException,
+              statusCode: error.response?.statusCode,
+              message: 'Connection not established',
+            );
+          case DioErrorType.sendTimeout:
+            return CustomException(
+              exceptionType: ExceptionType.SendTimeoutException,
+              statusCode: error.response?.statusCode,
+              message: 'Failed to send',
+            );
+          case DioErrorType.receiveTimeout:
+            return CustomException(
+              exceptionType: ExceptionType.ReceiveTimeoutException,
+              statusCode: error.response?.statusCode,
+              message: 'Failed to receive',
+            );
+          case DioErrorType.badResponse:
+          case DioErrorType.unknown:
+            if (error.message!.contains(ExceptionType.SocketException.name)) {
+              return CustomException(
+                exceptionType: ExceptionType.FetchDataException,
+                statusCode: error.response?.statusCode,
+                message: 'No internet connectivity',
+              );
+            }
+            if (error.response?.data['headers']['code'] == null) {
+              return CustomException(
+                exceptionType: ExceptionType.UnrecognizedException,
+                statusCode: error.response?.statusCode,
+                message: error.response?.statusMessage ?? 'Unknown',
+              );
+            }
+            final name = error.response?.data['headers']['code'] as String;
+            final message =
+                error.response?.data['headers']['message'] as String;
+            if (name == ExceptionType.TokenExpiredException.name) {
+              return CustomException(
+                exceptionType: ExceptionType.TokenExpiredException,
+                code: name,
+                statusCode: error.response?.statusCode,
+                message: message,
+              );
+            }
+            return CustomException(
+              message: message,
+              code: name,
+              statusCode: error.response?.statusCode,
+            );
+          case DioExceptionType.badCertificate:
+            return CustomException(
+              message: '',
+              code: '',
+              statusCode: error.response?.statusCode,
+            );
+          case DioExceptionType.connectionError:
+            return CustomException(
+              message: '',
+              code: '',
+              statusCode: error.response?.statusCode,
+            );
+        }
+      } else {
+        return CustomException(
+          exceptionType: ExceptionType.UnrecognizedException,
+          message: 'Error unrecognized',
+        );
+      }
+    } on FormatException catch (e) {
+      return CustomException(
+        exceptionType: ExceptionType.FormatException,
+        message: e.message,
+      );
+    } on Exception catch (_) {
+      return CustomException(
+        exceptionType: ExceptionType.UnrecognizedException,
+        message: 'Error unrecognized',
+      );
+    }
+  }
+
+  factory CustomException.fromParsingException(Exception error) {
+    // TODO(arafaysaleem): add logging to print stack trace
+    debugPrint('$error');
+    return CustomException(
+      exceptionType: ExceptionType.SerializationException,
+      message: 'Failed to parse network response to model or vice versa',
+    );
+  }
+}
